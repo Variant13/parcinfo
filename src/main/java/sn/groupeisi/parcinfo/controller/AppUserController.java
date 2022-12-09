147eda2b1dadfface691f687529a9f25afa70d9e
ousmane <ousmaneyaou00@gmail.com>
2022-12-09 03:58:03 +0100

    ajout de dockerfile et docker-compose

--- src/main/java/sn/groupeisi/parcinfo/controller/AppUserController.java
+++ src/main/java/sn/groupeisi/parcinfo/controller/AppUserController.java
@@ -1,7 +1,19 @@
 package sn.groupeisi.parcinfo.controller;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-
+import org.springframework.web.bind.annotation.GetMapping;
+@SpringBootApplication
 @Controller
 public class AppUserController {
+    @GetMapping("/")
+    public String home(){
+        return "Travail DevOps a trois";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppUserController.class, args);
+    }
+
 }
